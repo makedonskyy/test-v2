43f61aa385b9fa7cdd625b6a63384eb6be161d00
Yaroslava <yaroslavamakedonskaya@yandex.ru>
2022-11-15 22:30:00 +0300

    searching done

--- client/src/OneVacancy.jsx
+++ client/src/OneVacancy.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function OneVacancy({ vacancy }) {
-  console.log(vacancy);
+//   console.log(vacancy);
   return (
-    <div className="card">
+    <div className="card" style={{ marginTop: '50px' }}>
       <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        {/* <h6 className="card-subtitle mb-2 text-muted">{}</h6> */}
-        <p className="card-text">
-          Some quick example text to build on the card title
-          and make up the bulk of the cards content.
+        <h5 className="card-title">{vacancy.jobname[0]}</h5>
+        <h6 className="card-subtitle mb-2 text-muted">
+          {
+        `${vacancy.salary[0]} ${vacancy.currency}`
+}
 
-        </p>
-        <a href="#" className="card-link">Card link</a>
-        <a href="#" className="card-link">Another link</a>
+        </h6>
+        <Link to={`/vacancies/${vacancy.updatedate[0]}`} className="btn btn-secondary">Подробнее</Link>
       </div>
     </div>
   );
--- client/src/Vacancies.jsx
+++ client/src/Vacancies.jsx
@@ -1,20 +1,59 @@
 import React, { useEffect, useState } from 'react';
+// import Filter from './Filter';
 import OneVacancy from './OneVacancy';
 import vacancy from './test.json';
 
+const filterSearch = (search, vacanciesList) => {
+  if (!search) {
+    return vacanciesList;
+  }
+  return vacanciesList.filter((el) => el.jobname[0]
+    .toLowerCase().includes(search.toLowerCase()));
+};
+
 export default function Vacancies() {
-  // console.log(vacancy.source.vacancies[0].vacancy);
+  const data = vacancy.source.vacancies[0].vacancy;
+  const [list, setList] = useState(data);
+  const [serchingVacancies, setSerchingVacancies] = useState('');
+
+  useEffect(() => {
+    const Debounce = setTimeout(() => {
+      const filtered = filterSearch(serchingVacancies, data);
+      setList(filtered);
+    }, 300);
+
+    return () => clearTimeout(Debounce);
+  }, [serchingVacancies]);
+
   return (
-    <div className="row">
-      {
-        vacancy?.source?.vacancies[0]?.vacancy
-        && vacancy?.source?.vacancies[0]?.vacancy?.map((el) => (
+    <>
+      <div className="container">
+        <div className="container mx-auto font-mono">
+          <div className="text-3xl text-center py-3">Поиск вакансии</div>
+          <div className="flex justify-center flex-col align-middle">
+            <input
+              type="text"
+              autoComplete="off"
+              placeholder="Введите название"
+              onChange={(e) => setSerchingVacancies(e.target.value)}
+              className="w-100 text-stone-900 placeholder:italic placeholder:text-slate-400 block bg-white border border-slate-300 rounded-sm py-2 px-3 shadow-lg focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm mx-auto"
+              style={{
+                width: '600px',
+              }}
+            />
+            {
+        list
+        && list?.map((el) => (
           <OneVacancy
             vacancy={el}
             key={el.description}
           />
         ))
     }
-    </div>
+          </div>
+        </div>
+      </div>
+      {/* <Filter vacancy={vacancy} /> */}
+    </>
   );
 }
